97e39cfe5f5a99d01680473628dcb3aad304cc12
Vivian Quan <vivianquan@Vivians-MacBook-Pro.local>
2019-12-03 12:51:53 -0800

    Made some changes in client & server

--- server/js/httpHandler.js
+++ server/js/httpHandler.js
@@ -12,9 +12,63 @@ module.exports.initialize = (queue) => {
   messageQueue = queue;
 };
 
-module.exports.router = (req, res, next = ()=>{}) => {
+module.exports.router = (req, res, next = () => { }) => {
   console.log('Serving request type ' + req.method + ' for url ' + req.url);
   res.writeHead(200, headers);
   res.end();
   next(); // invoke next() at the end of a request to help with testing!
+
+  /*
+  if (req.method === 'OPTIONS') {
+    res.writeHead(200, headers);
+    res.end();
+  }
+
+  if (req.method === 'GET') {
+    if (req.url === '/') {
+      res.writeHead(200, headers);
+      var command = messageQueue.dequeue();
+      if (command) {
+        console.log('Responding with:', command);
+        res.end(command);
+      } else {
+        res.end();
+      }
+    }
+
+    if (req.url === '/background.jpg') {
+      fs.readFile(module.exports.backgroundImageFile, (err, fileData) => {
+        if (err) {
+          res.writeHead(404);
+        } else {
+          res.writeHead(200, {
+            'Content-Type': 'image/jepg',
+            'Content-Length': fileData.length
+          });
+          res.write(fileData, 'binary');
+        }
+        res.end();
+        next();
+      });
+    }
+  }
+
+  if (req.method === 'POST' && req.url === '/background.jpg') {
+    var imageData = Buffer.alloc(0);
+
+    req.on('data', (chunk) => {
+      imageData = Buffer.concat([imageData, chunk]);
+    });
+
+    req.on('end', () => {
+      var file = multipart.getFile(imageData);
+      fs.writeFile(module.exports.backgroundImageFile, file.data, (err) => {
+        res.writeHead(err ? 400 : 201, headers);
+        res.end();
+        next();
+      });
+    });
+  }
+
+  */
 };
